9da1609b4aa4bf07f7cf09d25460819f0de93afc
Egor Malyshev <malyshevegor1998@gmail.com>
2023-03-15 03:59:33 +0500

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import picture from './../pict/FC_Orenburg.png'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src={picture} alt="" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Content = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://planetofhotels.com/guide/sites/default/files/styles/node__blog_post__bp_banner/public/2020-06/Exotic-tropical-beach-sunse.jpg" alt="" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Content;
